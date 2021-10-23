a55d84f23416b0a5c4455ab4982094f1fd4fd725
Vadimka Komissarov <admin@vkom.cc>
2021-10-23 17:56:32 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"log/syslog"
+	"os"
+	"runtime"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+	"gopkg.in/urfave/cli.v1"
+)
+
+var log zerolog.Logger
+
+func main() {
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	log = zerolog.New(zerolog.ConsoleWriter{
+		Out: os.Stderr,
+	}).With().Timestamp().Logger()
+
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	log = log.Hook(SeverityHook{})
+
+	app := cli.NewApp()
+	app.Name = "NexusCloner"
+	app.Version = "0.1"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		{
+			Name:  "Vadimka K.",
+			Email: "admin@vkom.cc",
+		},
+	}
+	app.Copyright = "(c) 2021 mindhunter86"
+	app.Usage = "Repository cloning tool for nexus"
+
+	app.Flags = []cli.Flag{
+		// Some common options
+		cli.StringFlag{
+			Name:  "loglevel, l",
+			Value: "debug",
+			Usage: "log level (debug, info, warn, error, fatal, panic)",
+		},
+		cli.StringFlag{
+			Name:  "syslog-proto",
+			Value: "tcp",
+		},
+		cli.StringFlag{
+			Name:  "syslog-server",
+			Value: "",
+			Usage: "DON'T FORGET ABOUT TLS\\SSL, COMRADE",
+		},
+		cli.StringFlag{
+			Name:  "syslog-tag",
+			Value: "",
+		},
+		cli.IntFlag{
+			Name:  "clienttimeout, ct",
+			Value: 3,
+		},
+		cli.BoolFlag{
+			Name: "http-client-insecure",
+		},
+
+		// Application options
+	}
+	app.Action = func(c *cli.Context) (e error) {
+		log.Debug().Msg("prgm started")
+
+		// Usage: "log level (debug, info, warn, error, fatal, panic)",
+		switch c.String("loglevel") {
+		case "debug":
+			zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		case "info":
+			zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		case "warn":
+			zerolog.SetGlobalLevel(zerolog.WarnLevel)
+		case "error":
+			zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+		case "fatal":
+			zerolog.SetGlobalLevel(zerolog.FatalLevel)
+		case "panic":
+			zerolog.SetGlobalLevel(zerolog.PanicLevel)
+		default:
+			log.Warn().Str("input", c.String("loglevel")).Msg("Abnormal data has been given for loglevel!")
+		}
+
+		var sLog *syslog.Writer
+		if len(c.String("syslog-server")) != 0 {
+			log.Debug().Msg("Connecting to syslog server ...")
+
+			if sLog, e = syslog.Dial(
+				c.String("syslog-proto"),
+				c.String("syslog-server"),
+				syslog.LOG_INFO, // 2do put it into args
+				c.String("syslog-tag"),
+			); e != nil {
+				return
+			}
+
+			log.Debug().Msg("Syslog connection established! Reset zerolog for MultiLevelWriter set ...")
+
+			log = zerolog.New(zerolog.MultiLevelWriter(
+				zerolog.ConsoleWriter{
+					Out: os.Stderr,
+				},
+				sLog,
+			)).With().Timestamp().Logger()
+
+			log = log.Hook(SeverityHook{})
+
+			log.Info().Msg("Zerolog reinitialized! Starting commands...")
+		}
+
+		// Application starts here:
+		// return commands.NewCommands(&log).Bootstrap(c)
+		return nil
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	if e := app.Run(os.Args); e != nil {
+		log.Fatal().Err(e).Msg("")
+	}
+}
+
+type SeverityHook struct{}
+
+func (h SeverityHook) Run(e *zerolog.Event, level zerolog.Level, msg string) {
+	rfn := "unknown"
+	pcs := make([]uintptr, 1)
+
+	if runtime.Callers(4, pcs) != 0 {
+		if fun := runtime.FuncForPC(pcs[0] - 1); fun != nil {
+			rfn = fun.Name()
+		}
+	}
+
+	fn := strings.Split(rfn, "/")
+	e.Str("func", fn[len(fn)-1:][0])
+}
